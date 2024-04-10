20a4447e50a6ced1be2d278b6952dd3982ba1be6
nogodmessi <yfshangan@163.com>
2024-04-10 13:42:10 +0800

    清空购物车购物车业务代码(C端)

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -32,6 +32,14 @@ public class ShoppingCartController {
     }
 
     /**
+     * 删除购物车
+     * @return
+     */
+    public Result sub(){
+        return Result.success();
+    }
+
+    /**
      * 查看购物车
      * @return
      */
@@ -40,5 +48,17 @@ public class ShoppingCartController {
     public Result<List<ShoppingCart>> list(){
         List<ShoppingCart> list = shoppingCartService.showShoppingCart();
         return Result.success(list);
+    }
+
+    /**
+     * 清空购物车
+     * @return
+     */
+
+    @DeleteMapping("/clean")
+    @ApiOperation("清空购物车")
+    public Result clean(){
+        shoppingCartService.cleanShoppingCart();
+        return Result.success();
     }
 }
