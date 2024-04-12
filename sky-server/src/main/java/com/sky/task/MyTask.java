4e8c401327a5490b39dcba8af26da4ee32746b6c
nogodmessi <yfshangan@163.com>
2024-04-13 00:53:19 +0800

    定时功能实现

---
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,21 @@
+package com.sky.task;
+
+/**
+ * @author 杨帆
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自定义定时任务类
+ */
+@Component
+@Slf4j
+public class MyTask {
+
+    /**
+     * 定时任务
+     */
+
+}
